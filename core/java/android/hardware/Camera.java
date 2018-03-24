ffa3014b02faee58a9a030c85e2684de04d02a9f
Rashed Abdel-Tawab <rashed@linux.com>
2018-03-24 13:20:52 -0400

    camera: Check if aux camera whitelist is set before restricting cameras
    Some devices have a fully featured 3rd camera and adding 20+ camera apps
    to the whitelist is impossible due to the string length limit on
    systemprops. Add a check to see if the prop is even set, and if not,
    check if the blacklist property is set and mark those apps to hide the
    3rd camera from. If a package is not part of the blacklist, just restore
    the original behaviour and expose all the cameras to the app.
    
    Change-Id: I6c3b33c077e8710c73b5d0fa28e1b017d6c43a58

--- core/java/android/hardware/Camera.java
+++ core/java/android/hardware/Camera.java
@@ -288,7 +288,52 @@ public class Camera {
      * @return total number of accessible camera devices, or 0 if there are no
      *   cameras or an error was encountered enumerating them.
      */
-    public native static int getNumberOfCameras();
+    public static int getNumberOfCameras() {
+        /* Force to expose only two cameras
+         * if the package name does not falls in this bucket
+         */
+        int numberOfCameras = native_getNumberOfCameras();
+        if ((numberOfCameras > 2) && !shouldExposeAuxCamera()) {
+            numberOfCameras = 2;
+        }
+        return numberOfCameras;
+    }
+
+    /**
+     * Wether to expose Aux cameras
+     */
+    /** @hide */
+    public static boolean shouldExposeAuxCamera() {
+        String packageName = ActivityThread.currentOpPackageName();
+        // This should be .packagewhitelist but we shouldn't change qualcomm's default
+        String packageList = SystemProperties.get("vendor.camera.aux.packagelist");
+        String packageBlacklist = SystemProperties.get("vendor.camera.aux.packageblacklist");
+        if (packageList.length() > 0) {
+            TextUtils.StringSplitter splitter = new TextUtils.SimpleStringSplitter(',');
+            splitter.setString(packageList);
+            for (String str : splitter) {
+                if (packageName.equals(str)) {
+                    return true;
+                }
+            }
+            return false;
+        } else if (packageBlacklist.length() > 0) {
+            TextUtils.StringSplitter splitter = new TextUtils.SimpleStringSplitter(',');
+            splitter.setString(packageBlacklist);
+            for (String str : splitter) {
+                if (packageName.equals(str)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Returns the number of physical cameras available on this device.
+     */
+    /** @hide */
+    public native static int native_getNumberOfCameras();
 
     /**
      * Returns the information about a particular camera.
