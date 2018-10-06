13d04a52fd039e76df7787e655bb1b23be4912fa
Dominik Prochazka <mcmovieslovakiasro@gmail.com>
2018-10-06 14:15:33 +0200

    Revert "Add quick unlock [1/2]"
    This reverts commit 0a25c1d53a700b46c0bd3faba748a475807238e8.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3100,14 +3100,7 @@ public final class Settings {
          * @hide
          */
          
-        /**
-         * Whether to use the custom quick unlock screen control
-         * @hide
-         */
-        public static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL =
-                "lockscreen_quick_unlock_control";
-		 
-		 public static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
+        public static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
 
         /**
          * A formatted string of the next alarm that is set, or the empty string
